1a5ca8d24fd60fb70af33775834cb16aee2a0fbf
cuisongliu <cuisongliu@qq.com>
2023-07-19 14:26:20 +0800

    feature(main): add helm chart release for openIM
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- helm.sh
+++ helm.sh
@@ -9,12 +9,13 @@ version=$1
 if [[ -z "$version" ]]; then
   # 获取最新release的版本号
   version=$(curl -s "https://api.github.com/repos/$repository/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+  # 构建下载链接
+  download_url="https://github.com/$repository/releases/download/$version/openIM-${version#v}.tgz"
+  # 下载指定版本的release
+  wget $download_url
+  helm repo index . --url https://github.com/$repository/releases/download/$version
+  exit 0
 fi
-
-# 构建下载链接
-download_url="https://github.com/$repository/releases/download/$version/openIM-${version#v}.tgz"
-
-# 下载指定版本的release
-wget $download_url
-
-helm repo index . --url https://github.com/$repository/releases/download/$version
+sed -i "s#default#latest#g" charts/openIM/Chart.yaml
+helm package charts/openIM
+helm repo index . --url https://github.com/$repository
